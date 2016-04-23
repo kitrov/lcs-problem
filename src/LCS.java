635048a48327614057fb7324c2d546adfbf0810d
Viktor <justtheviktor@gmail.com>
2016-04-23 11:51:52 +0300

    Update 0

---
+++ src/LCS.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Vik on 4/23/2016.
+ */
+public class LCS {
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Vik on 4/23/2016.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("LCS problem implementation");
+    }
+}
+
